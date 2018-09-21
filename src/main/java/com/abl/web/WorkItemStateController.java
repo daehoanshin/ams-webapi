ca06b6d12233f04130544f9926c2256c0f2183a0
DaeHoan Shin <xbb123@naver.com>
2018-09-21 15:24:44 +0900

    rtc library 추가전

--- src/main/java/com/abl/web/WorkItemStateController.java
+++ src/main/java/com/abl/web/WorkItemStateController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,5 +21,14 @@ public class WorkItemStateController {
 		model.addAttribute("userId", userId);
 		model.addAttribute("password", password);
 		return "test";
+	}
+	
+	@RequestMapping(value = "update/{wiid}", method = RequestMethod.GET)
+	public String update(@PathVariable Long wiid, Model model, String userId, String password, HttpSession session) {
+		System.out.println("id :" + wiid);
+		model.addAttribute("wiid", wiid);
+		model.addAttribute("userId", userId);
+		model.addAttribute("password", password);
+		return "update";
 	}
 }
--- webapp/WEB-INF/views/test.jsp
+++ webapp/WEB-INF/views/test.jsp
@@ -7,7 +7,17 @@
 <title>Insert title here</title>
 </head>
 <body>
+
+wiid :  ${wiid}
 userId :  ${userId}
 password : ${password }
+
+
+<form>
+<input type="button" value="Put Your Text Here" onclick="window.location.href='http://localhost:8801/update/22/?userId=dhshin'" />
+</form>
+
+<input type="button" value="Put Your Text Here" onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
 </body>
 </html>
